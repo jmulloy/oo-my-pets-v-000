7d5773dbc41f35527587e541aaaf584cadc466ae
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-10 01:04:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :mood
   attr_reader :name
 
-    def initialize(name, mood = "nervous")
+    def initialize(name, mood = "happy")
       @name = name
       @mood = mood
     end
