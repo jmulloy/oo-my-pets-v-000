fe97b94c3d35ee94fb5508aaa40fe4e941ec0a22
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-10 00:58:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -16,12 +16,12 @@ attr_reader :species, :pets
     self.all.count
   end
 
-  def initialize(mood, species = "hamster")
+  def initialize(species = "hamster")
 
     @@all << self
     @species = species
     @pets = {:fishes => [], :dogs => [], :cats => []}
-    @mood = mood
+    
   end
 
   def say_species
